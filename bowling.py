cd94adec7c736276ee22922f6343f339723a9a57
AttilaMuller <AttilaMuller@github.com>
2017-11-28 13:14:38 +0100

    Renamed and declared variable last in score function and renamed char variable in score and get_value functions

--- bowling.py
+++ bowling.py
@@ -1,41 +1,42 @@
 def score(game):
     result = 0
+    previous_score = 0
     frame = 1
     in_first_half = True
-    for i, char in enumerate(game):
-        if char == '/':
-            result += 10 - last
+    for i, character in enumerate(game):
+        if character == '/':
+            result += 10 - previous_score
         else:
-            result += get_value(char)
-        if frame < 10 and get_value(char) == 10:
-            if char == '/':
+            result += get_value(character)
+        if frame < 10 and get_value(character) == 10:
+            if character == '/':
                 result += get_value(game[i+1])
-            elif char == 'X' or char == 'x':
+            elif character == 'X' or character == 'x':
                 result += get_value(game[i+1])
                 if game[i+2] == '/':
                     result += 10 - get_value(game[i+1])
                 else:
                     result += get_value(game[i+2])
-        last = get_value(char)
+        previous_score = get_value(character)
         if in_first_half is False:
             frame += 1
         if in_first_half is True:
             in_first_half = False
         else:
             in_first_half = True
-        if char == 'X' or char == 'x':
+        if character == 'X' or character == 'x':
             in_first_half = True
             frame += 1
     return result
 
 
-def get_value(char):
+def get_value(character):
 
-    if char == 'X' or char == 'x' or char == '/':
+    if character == 'X' or character == 'x' or character == '/':
         return 10
-    elif char == '-':
+    elif character == '-':
         return 0
-    elif int(char) in range(1, 10):
-        return int(char)
+    elif int(character) in range(1, 10):
+        return int(character)
     else:
         raise ValueError()
